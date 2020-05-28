5452861f4b007a5b4ee0324c1b4ae99ad5432976
Jorge <al050610@uacam.mx>
2020-05-27 19:40:01 -0500

    Agregado Requerimientos del Curso, No terminado

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,9 @@ Route::group(['prefix' => 'courses'], function () {
         Route::post('/store', 'CourseController@store')
             ->name('courses.store')
             ->middleware([sprintf("role:%s", \App\Role::TEACHER)]);
+        Route::put('/{course}/update', 'CourseController@update')
+            ->name('courses.update')
+            ->middleware([sprintf("role:%s", \App\Role::TEACHER)]);
     });
 
     Route::get('/{course}', 'CourseController@show')->name('courses.detail');
