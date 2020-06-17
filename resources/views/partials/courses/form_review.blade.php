5ec06a1e6c02e1e04249bb791a1979d5ad575c60
DeLaCrucito <al053938@uacam.mx>
2020-06-16 23:21:06 -0500

    Oscar. bloquear videos a los no registrados

--- resources/views/partials/courses/form_review.blade.php
+++ resources/views/partials/courses/form_review.blade.php
@@ -1,14 +1,15 @@
 @cannot('inscribe', $course)
-
-    <div class="col-12 pt-0 mt-4">
-        <h2 class="text-muted">{{ __("Videos del Profesor") }}</h2>
-        <hr/>
-    </div>
-    <div class="col-12">
-        <p class="mb-0">
-        <div class="embed-responsive embed-responsive-16by9">
-            <iframe class="embed-responsive-item" src="{{ $course->youtube_url}}" allowfullscreen></iframe>
+    @auth
+        <div class="col-12 pt-0 mt-4">
+            <h2 class="text-muted">{{ __("Videos del Profesor") }}</h2>
+            <hr/>
         </div>
-        </p>
-    </div>
+        <div class="col-12">
+            <p class="mb-0">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="{{ $course->youtube_url}}" allowfullscreen></iframe>
+            </div>
+            </p>
+        </div>
+    @endauth
 @endcannot
